8ae486c2f474ecaa4eee80f7436cce61c559188d
Keerthana <keerthanad.it2022@citchennai.net>
2024-12-19 13:25:13 +0530

    Add .gitattributes to manage line endings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Form from './components/Form';
+import Table from './components/Table';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/employees" element={<Table />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
